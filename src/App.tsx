e58080acb943537f9b19ba940a282b312cf5253a
David Ivain <d.ivain@outlook.fr>
2022-03-12 16:56:03 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Component, createSignal } from 'solid-js';
+import './App.scss';
+
+const App: Component = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello, wordl!</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
