1ba20f9ff4cee8ccd12436b2e4cec8e24bb6b7bc
David Ivain <d.ivain@outlook.fr>
2022-03-23 17:24:23 +0100

    added window resizing

--- src/Components/Window/Window.tsx
+++ src/Components/Window/Window.tsx
@@ -6,16 +6,27 @@ import { MockApp } from "../../types";
 import SVG from "../SVG/SVG";
 import "./_window.scss";
 
+function WindowResizer({ position, start, end }: { position: string, start: (e: MouseEvent | TouchEvent) => void, end: (e: MouseEvent | TouchEvent) => void }) {
+    return (
+        <div className="c-window__resize-area" data-position={position} onMouseDown={start} onMouseUp={end} onTouchStart={start} onTouchEnd={end}></div>
+    );
+}
 
 function Window({ children, x = 32, y = 32, width = 300, height = 200, appID }: PropsWithChildren<{ x?: number, y?: number, width?: number, height?: number, appID: string }>) {
     const [windowPosition, setWindowPosition] = createSignal<{ x: number, y: number }>({ x: x, y: y });
+    const [windowSize, setWindowSize] = createSignal({ width, height });
+    const [isMaximized, setIsMaximized] = createSignal(false);
+
     const [dragOffset, setDragOffset] = createSignal<{ x: number, y: number }>({ x: 0, y: 0 });
     const [isDragging, setIsDragging] = createSignal(false);
-    const [isMaximized, setIsMaximized] = createSignal(false);
+    const [isResizing, setIsResizing] = createSignal(false);
+
     const [isTouch, setIsTouch] = createSignal(false);
 
     let clickTime = 0;
     let dragAnimationFrame;
+    let resizeAnimationFrame;
+    let resizePositions = { top: false, right: false, bottom: false, left: false };
 
     const appsContext = useApps();
     const apps = appsContext.apps,
@@ -28,6 +39,7 @@ function Window({ children, x = 32, y = 32, width = 300, height = 200, appID }: 
     const { position, touchPosition } = useMouse();
 
     document.addEventListener('mouseup', endDragging);
+    document.addEventListener('mouseup', endResizing);
 
     function startDragging(e: MouseEvent | TouchEvent) {
         if (e instanceof MouseEvent) e.preventDefault();
@@ -64,15 +76,63 @@ function Window({ children, x = 32, y = 32, width = 300, height = 200, appID }: 
         setIsDragging(false);
     }
 
+    function startResizing(e: MouseEvent | TouchEvent) {
+        if (e instanceof MouseEvent) e.preventDefault();
+        if (isMaximized()) return;
+        clickTime = e.timeStamp;
+        let resizePosition = (e.currentTarget as HTMLElement).dataset.position;
+        resizePositions.top = ["top", "top-left", "top-right"].includes(resizePosition);
+        resizePositions.right = ["right", "top-right", "bottom-right"].includes(resizePosition);
+        resizePositions.bottom = ["bottom", "bottom-left", "bottom-right"].includes(resizePosition);
+        resizePositions.left = ["left", "top-left", "bottom-left"].includes(resizePosition);
+        if (e instanceof MouseEvent) {
+            setIsTouch(false);
+            setDragOffset({ x: e.pageX - windowPosition().x, y: e.pageY - windowPosition().y });
+        }
+        else {
+            setIsTouch(true);
+            setDragOffset({ x: e.touches[0].pageX - windowPosition().x, y: e.touches[0].pageY - windowPosition().y });
+        }
+        //setIsDragging(true);
+        resizeAnimationFrame = requestAnimationFrame(resize)
+    }
+
+    function resize(time: number) {
+        setIsResizing(true);
+        const mouseXY = !isTouch() ? position() : touchPosition();
+        const wp = windowPosition();
+        let newWindowPosition = { ...wp };
+        let newWindowSize = windowSize();
+        if (resizePositions.top) {
+            newWindowPosition.y = mouseXY.y - dragOffset().y;
+            newWindowSize.height -= newWindowPosition.y - wp.y;
+        }
+        if (resizePositions.right) newWindowSize.width = mouseXY.x - windowPosition().x;
+        if (resizePositions.left) {
+            newWindowPosition.x = mouseXY.x - dragOffset().x;
+            newWindowSize.width -= newWindowPosition.x - wp.x;
+        }
+        if (resizePositions.bottom) newWindowSize.height = mouseXY.y - windowPosition().y;
+        setWindowPosition({ ...newWindowPosition });
+        setWindowSize({ ...newWindowSize });
+        resizeAnimationFrame = requestAnimationFrame(resize);
+    }
+
+    function endResizing(e: MouseEvent | TouchEvent) {
+        cancelAnimationFrame(resizeAnimationFrame);
+        setIsResizing(false);
+    }
+
     function maximize(e) {
         setIsMaximized(!isMaximized());
     }
 
     return (
         <div class={`c-window${isMaximized() ? ' c-window--maximized' : ''}${activeWindow() === appID ? ' c-window--active' : ''}`}
-            style={`${app().defaultPosition.alignY}: ${isMaximized() ? 0 : windowPosition().y}px; width: ${isMaximized() ? '100vw' : `${width}px`}; height: ${isMaximized() ? '100%' : `${height}px`}; ${app().defaultPosition.alignX}: ${isMaximized() ? 0 : windowPosition().x}px;`}
+            style={`${app().defaultPosition.alignY}: ${isMaximized() ? 0 : windowPosition().y}px; width: ${isMaximized() ? '100vw' : `${windowSize().width}px`}; height: ${isMaximized() ? '100%' : `${windowSize().height}px`}; ${app().defaultPosition.alignX}: ${isMaximized() ? 0 : windowPosition().x}px;`}
             onMouseDown={() => focus(appID)}
-            data-dragged={isDragging()}>
+            data-dragged={isDragging()}
+            data-resizing={isResizing()}>
             {app().frameless ? <></>
                 : (<div class={`c-window__header${app().windowColorValue.luminance < 0.5 ? ' c-window__header--dark' : ''}`} style={`background-color: ${app().windowColor};`}>
                     <div class="c-window__title" onMouseDown={startDragging} onMouseUp={endDragging} onTouchStart={startDragging} onTouchEnd={endDragging} onDblClick={maximize}>
@@ -94,6 +154,14 @@ function Window({ children, x = 32, y = 32, width = 300, height = 200, appID }: 
             <div className="c-window__content">
                 {children}
             </div>
+            <WindowResizer position="top" start={startResizing} end={endResizing} />
+            <WindowResizer position="right" start={startResizing} end={endResizing} />
+            <WindowResizer position="bottom" start={startResizing} end={endResizing} />
+            <WindowResizer position="left" start={startResizing} end={endResizing} />
+            <WindowResizer position="top-left" start={startResizing} end={endResizing} />
+            <WindowResizer position="top-right" start={startResizing} end={endResizing} />
+            <WindowResizer position="bottom-right" start={startResizing} end={endResizing} />
+            <WindowResizer position="bottom-left" start={startResizing} end={endResizing} />
         </div>
     )
 }
